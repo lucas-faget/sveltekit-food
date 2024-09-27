e26a8ced835c933f31847316fd7b3966c362da3e
lucas-faget <lucas.faget@laposte.net>
2024-09-27 14:44:58 +0200

    Adding tailwindcss and iconify

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
+<script>
+    import Icon from "@iconify/svelte";
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<Icon icon="mdi-light:home" />
