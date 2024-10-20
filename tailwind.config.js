13b96bc7fc88ad117b882e71827b10ac8d4e2ffa
lucas-faget <lucas.faget@laposte.net>
2024-10-20 19:44:05 +0200

    Adding Flowbite Svelte

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+import flowbitePlugin from "flowbite/plugin";
+
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ["./src/**/*.{html,js,svelte,ts}"],
+    content: [
+        "./src/**/*.{html,js,svelte,ts}",
+        "./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}",
+    ],
+    darkMode: "selector",
     theme: {
         extend: {},
     },
-    plugins: [],
+    plugins: [flowbitePlugin],
 };
